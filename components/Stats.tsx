63e4eb813cf07ffc96e5fa40589b085bac489a7e
emmaccen <oriolaemmanuel199@gmail.com>
2025-08-15 22:41:38 +0100

    App stats

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -1,5 +1,8 @@
 import { MessageSquare, Target, TrendingUp, Zap } from "lucide-react"
-import React from "react"
+import React, { useEffect, useState } from "react"
+
+import { useGlobalState } from "~store/GlobalContext"
+import type { TemplateCategory } from "~types"
 
 type StatCardProps = {
   label: string
@@ -18,28 +21,63 @@ function StatCard({ label, value, icon }: StatCardProps) {
     </div>
   )
 }
-
+type Stats = {
+  totalTemplates: number
+  activeTemplates: number
+  categories: number
+  activeCategories: number
+}
 export const Stats = () => {
+  const [statsData, setStatsData] = useState<Stats>()
+  const { templates } = useGlobalState()
+  const provideStats = (templates: Record<string, TemplateCategory>) => {
+    let stats: Stats = {
+      totalTemplates: 0,
+      activeTemplates: 0,
+      categories: 0,
+      activeCategories: 0
+    }
+    stats["totalTemplates"] = Object.values(templates).reduce(
+      (acc, category) => acc + category.templates.length,
+      0
+    )
+    stats["activeTemplates"] = Object.values(templates).reduce(
+      (acc, category) =>
+        acc + category.templates.filter((t) => t.active).length,
+      0
+    )
+    stats["categories"] = Object.keys(templates).length
+    stats["activeCategories"] = Object.values(templates).filter(
+      (category) => category.active
+    ).length
+
+    return stats
+  }
+
+  useEffect(() => {
+    if (templates) setStatsData(provideStats(templates))
+  }, [templates])
+
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
       <StatCard
         label="Total Templates"
-        value={10}
+        value={statsData?.totalTemplates || 0}
         icon={<MessageSquare className="text-blue-500" />}
       />
       <StatCard
         label="Active Templates"
-        value={77}
+        value={statsData?.activeTemplates || 0}
         icon={<Zap className="text-green-500" />}
       />
       <StatCard
         label="Categories"
-        value={2}
+        value={statsData?.categories || 0}
         icon={<Target className="text-purple-500" />}
       />
       <StatCard
         label="Active Categories"
-        value={8}
+        value={statsData?.activeCategories || 0}
         icon={<TrendingUp className="text-orange-500" />}
       />
     </div>
